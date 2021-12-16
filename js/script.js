ee3a8f91f16bae060329e47918ea8cbfacdc8eed
Rafael <khachikyan38@gmail.com>
2021-12-16 17:24:36 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const body = document.body;
 const box = document.querySelector(".js-checkbox");
-const title = document.querySelector(".theme__title");
-const span = document.querySelector(".theme__span");
+const title = document.querySelector(".social__title");
+const span = document.querySelector(".social__span");
 
 box.addEventListener("click", () => {
   if (!body.classList.contains("dark__mode")) {
@@ -9,14 +9,11 @@ box.addEventListener("click", () => {
     body.classList.add("dark__mode");
     box.setAttribute("checked", true);
     span.classList.add("switch__active");
-    title.innerHTML = "Go Light";
   } else {
     body.classList.remove("dark__mode");
     body.classList.add("light__mode");
     box.removeAttribute("checked");
     span.classList.remove("switch__active");
-
-    title.innerHTML = "Go Dark";
   }
 });
 
@@ -26,12 +23,12 @@ function checkPreference() {
   if (preference_query.matches && window.matchMedia) {
     body.classList.remove("light__mode");
     body.classList.add("dark__mode");
-    title.innerHTML = "Go Light";
+
     span.classList.add("switch__active");
   } else {
     body.classList.remove("dark__mode");
     body.classList.add("light__mode");
-    title.innerHTML = "Go Dark";
+
     span.classList.remove("switch__active");
   }
 }
